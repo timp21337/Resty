b4ec9d7abeb2542e24e3c4be4b817645dcaf7424
timp21337 <timp21337@paneris.org>
2014-01-20 14:34:23 +0000

    Do not override toString

--- src/main/java/us/monoid/web/FormContent.java
+++ src/main/java/us/monoid/web/FormContent.java
@@ -9,25 +9,18 @@ import java.io.UnsupportedEncodingException;
  *
  */
 public class FormContent extends Content {
-	protected String rawQuery;
 
 	public FormContent(String query) {
 		super("application/x-www-form-urlencoded", getBytes(query)); // strictly speaking US ASCII should be used
-	}
+
+  }
 	
 	private static byte[] getBytes(String query) {
 		try {
 			return query.getBytes("UTF-8");
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-		return new byte[0]; // should never happen
 	}
 
-
-	@Override
-	public String toString() {
-		return rawQuery;
-	}
 }
